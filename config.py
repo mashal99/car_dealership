763b24f938d0830568e3713803d50da1c3ce5573
Ahmed Mashaal <46572957+mashal99@users.noreply.github.com>
2024-12-14 19:12:13 -0500

    add mysql info

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'  # SQLite for simplicity
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:"root@123"@localhost:3306/car_dealership'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_secret_key'
